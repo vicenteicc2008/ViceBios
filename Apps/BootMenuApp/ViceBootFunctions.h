95d7cad69688e3968648382e7c714f80963c3620
Vicente Cortes <vicenteicc2008@gmail.com>
2024-01-28 20:29:25 -0300

    add source code from UefiBootMenu
    credits

---
+++ Apps/BootMenuApp/ViceBootFunctions.h
@@ -0,0 +1,28 @@
+#ifndef H_VICE_MEOWFUNCTIONS
+#define H_VICE_MEOWFUNCTIONS
+
+#include <Protocol/DevicePath.h>
+
+EFI_STATUS DrawString( CHAR16 *string, const EFI_FONT_DISPLAY_INFO *fontDisplayInfo,
+	EFI_IMAGE_OUTPUT *imageOutput, UINTN x, UINTN y );
+	
+EFI_STATUS DrawLines( CHAR16 *string, const EFI_FONT_DISPLAY_INFO *fontDisplayInfo,
+	EFI_IMAGE_OUTPUT *imageOutput, UINTN x, UINTN y );
+
+void Log( IN CHAR16 *text );
+
+UINT32 SprintUint( UINT32 decimal, CHAR16 *buffer, UINT32 offset );
+
+UINT32 Max( UINT32 one, UINT32 another );
+
+EFI_EVENT GetKeyWaitor();
+
+EFI_STATUS GetOneSecWaitor( OUT EFI_EVENT *event );
+
+EFI_STATUS CancelTimer( EFI_EVENT *event );
+
+EFI_STATUS GetKey( EFI_INPUT_KEY *key );
+
+CHAR16 *VicePathToText( EFI_DEVICE_PATH_PROTOCOL *path );
+
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ViceBios
 a BIOS Setup for UEFI
+
+## Credits
+oO0oO0oO0o0o00 for UefiBootMenu
